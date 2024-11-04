8fa9034baccf13cde9f0fec9d9cdeb5fa9d8cd6d
MoatazSobhy <moatazsobhy2021@gmail.com>
2024-11-04 14:14:09 +0200

    add workflow

--- odoo/custom_addons/app_one/models/property.py
+++ odoo/custom_addons/app_one/models/property.py
@@ -1,3 +1,5 @@
+from email.policy import default
+
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
@@ -26,6 +28,11 @@ class Property(models.Model):
     ], default='north')
     owner_id = fields.Many2one('owner')  # database relation
     tag_ids = fields.Many2many('tag')
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('pending', 'Pending'),
+        ('sold', 'Sold'),
+    ], default='draft')
 
     _sql_constraints = [
         ('unique_name', 'unique("name")', 'This name is exist!'),
