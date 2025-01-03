4b9e03c4ef9c217ea935cc1814b3806377ebc694
MoatazSobhy <moatazsobhy2021@gmail.com>
2025-01-04 00:56:39 +0200

    APIs in Odoo 'create operation2'

--- odoo/custom_addons/app_one/controllers/property_api.py
+++ odoo/custom_addons/app_one/controllers/property_api.py
@@ -7,8 +7,31 @@ class PropertyApi(http.Controller):
     def post_property(self):
         args = request.httprequest.data.decode()  # get the json body
         vals = json.loads(args)                   # convert json to dictionary
+        if not vals.get('name'):
+            return request.make_json_response({
+                "message": "Name is required",
+            }, status=400)
+        try:
+            res = request.env['property'].sudo().create(vals)
+            if res:
+                return request.make_json_response({
+                    "message": "Property created successfully",
+                    "id": res.id,
+                    "name": res.name
+                }, status=201)
+        except Exception as error:
+            return request.make_json_response({
+                "message": error,
+            }, status=400)
+
+    # this is not the best case
+    @http.route("/v1/property/json", methods=["POST"], type="json", auth="none", csrf=False)
+    def post_property_json(self):
+        args = request.httprequest.data.decode()  # get the json body
+        vals = json.loads(args)  # convert json to dictionary
         res = request.env['property'].sudo().create(vals)
         if res:
-            return request.make_json_response({
-                "message": "Property created successfully"
-            }, status=200)
+            return ({
+                "message": "Property created successfully",
+            })
+
